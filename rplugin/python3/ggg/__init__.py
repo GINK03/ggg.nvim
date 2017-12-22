92de9b837f6432d6f9bb18479ac0422d8084715d
nardtree <gim.kobayashi@gmail.com>
2017-12-23 03:48:06 +0900

    add

--- rplugin/python3/ggg/__init__.py
+++ rplugin/python3/ggg/__init__.py
@@ -52,7 +52,7 @@ class ggg(object):
     ret = self.nvim.eval('@0')
     '''escape処理'''
     try:
-      self.nvim.command(shlex.quote("echo '[yankPost@GGG]\n{ret}'".format(ret=ret)))
+      self.nvim.out_write('{ret}'.format(ret=ret))
     except neovim.api.nvim.NvimError as ex:
       ...
 
