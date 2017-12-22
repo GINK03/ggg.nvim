56b3e514188101370d99367a43a78ee7640af3de
nardtree <gim.kobayashi@gmail.com>
2017-12-22 19:13:30 +0900

    a

--- rplugin/python3/ggg/__init__.py
+++ rplugin/python3/ggg/__init__.py
@@ -1,11 +1,13 @@
 import neovim
 import time
+import os
 
 @neovim.plugin
 class ggg(object):
   def __init__(self, nvim):
     self.nvim = nvim
-  
+    self.nvim.command("echo '非同期でgggプラグインが有効になりました'")
+    
   @neovim.command("GGGEcho")
   def echo(self):
     self.nvim.command("echo '[Echo Test]'")
