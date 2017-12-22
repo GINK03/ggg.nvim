d75c2615846f604562535df70060909fe13f49e1
gimpei <gim.kobayashi@gmail.com>
2017-12-22 20:03:41 +0900

    a

--- rplugin/python3/ggg/__init__.py
+++ rplugin/python3/ggg/__init__.py
@@ -21,7 +21,7 @@ class ggg(object):
   def git(self):
     self.nvim.command("echo '[Load Git Repo function@GGG]'")
     add = os.popen('git add *').read()
-    #self.nvim.command("echo '{}'".format(add))
+    self.nvim.command("echo '{}'".format(add))
     #commit = os.popen('git commit -m "add"').read()
     #self.nvim.command("echo '{}'".format(commit))
     #push = os.popen('git push').read()
